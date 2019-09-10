305369f38b592f26c880ee202afd1f076eedc762
Keith Walker <Keithrwalker418@gmail.com>
2019-09-10 18:45:28 -0500

    update 9-10

---
+++ Week_2/CorporateClass/CorporateClass/Classes/Employees/Boss.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorporateClass.Classes.Employees
+{
+    class Boss : Employee
+    {
+        public bool IsAdmin => true;
+        public Boss(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            Salary = 1000000;
+        }
+    }
+}
---
+++ Week_2/CorporateClass/CorporateClass/Classes/Employees/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorporateClass.Classes.Employees
+{
+    class Manager : Employee
+    {
+        public Manager(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            Salary = 50000;
+        }
+    }
+}
