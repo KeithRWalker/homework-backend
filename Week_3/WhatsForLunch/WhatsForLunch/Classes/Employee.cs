94f25320b0f024afa854d8f6f6a1573a003b3dee
Keith Walker <Keithrwalker418@gmail.com>
2019-09-13 18:05:14 -0500

    whats for lunch

---
+++ Week_3/WhatsForLunch/WhatsForLunch/Classes/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhatsForLunch.Classes
+{
+    class Employee
+    {
+        public string Name { get; set; }
+
+        public Employee(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Week_3/WhatsForLunch/WhatsForLunch/Classes/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhatsForLunch.Classes
+{
+    class Restaurant
+    {
+        public string Name { get; }
+
+        private List<string> _nameList => new List<string> { "Burger King", "Taco Bell", "McDonald's", "Chik'Fil'A", "Panda Express", "Little Ceasar's", "Cook Out", "The Bar" };
+
+        public Restaurant()
+        {
+            var index = new Random().Next(_nameList.Count);
+            Name = _nameList[index];
+        }
+    }
+}
