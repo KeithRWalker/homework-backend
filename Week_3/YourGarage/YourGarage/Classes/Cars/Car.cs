1dc0e186e1d0812a7a242680196ee476002a28c6
Keith Walker <Keithrwalker418@gmail.com>
2019-09-13 20:37:24 -0500

    done

---
+++ Week_3/YourGarage/YourGarage/Classes/Cars/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YourGarage.Classes
+{
+    class Car : Vehicle
+    {
+        public Car()
+        {
+            PassengerOcc = 5;
+            FuelCap = 20;
+            TopSpeed = 120;
+        }
+
+        public override void Drive(int distance)
+        {
+            OnRoad = true;
+            base.Drive(distance);
+        }
+    }
+}
---
+++ Week_3/YourGarage/YourGarage/Classes/Cars/SUV.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace YourGarage.Classes.Cars
+{
+    class SUV : Car
+    {
+        public SUV(string color)
+        {
+            Color = color;
+            PassengerOcc = 8;
+            FuelCap = 35;
+            TopSpeed = 130;
+        }
+    }
+}
